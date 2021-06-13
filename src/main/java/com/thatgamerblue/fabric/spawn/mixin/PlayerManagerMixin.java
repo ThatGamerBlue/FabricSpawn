d0023ba97a4c647040beb57dafd0fa158753ef0b
ThatGamerBlue <thatgamerblue@gmail.com>
2021-06-13 15:04:20 +0100

    only set facing if respawn world is same as config world

--- src/main/java/com/thatgamerblue/fabric/spawn/mixin/PlayerManagerMixin.java
+++ src/main/java/com/thatgamerblue/fabric/spawn/mixin/PlayerManagerMixin.java
@@ -1,23 +1,30 @@
 package com.thatgamerblue.fabric.spawn.mixin;
 
 import com.thatgamerblue.fabric.spawn.SpawnCommand;
+import java.util.Optional;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin
 {
-	@Inject(at = @At("TAIL"), method = "respawnPlayer(Lnet/minecraft/server/network/ServerPlayerEntity;Z)Lnet/minecraft/server/network/ServerPlayerEntity;")
-	public void postRespawnPlayer(ServerPlayerEntity player, boolean alive, CallbackInfoReturnable<ServerPlayerEntity> cir)
+	@Inject(at = @At("TAIL"), method = "respawnPlayer(Lnet/minecraft/server/network/ServerPlayerEntity;Z)Lnet/minecraft/server/network/ServerPlayerEntity;", locals = LocalCapture.CAPTURE_FAILHARD)
+	public void postRespawnPlayer(ServerPlayerEntity player, boolean alive, CallbackInfoReturnable<ServerPlayerEntity> cir,
+		// locals
+		BlockPos blockPos, float f, boolean bl, ServerWorld serverWorld, Optional optional2, ServerWorld serverWorld2, ServerPlayerEntity serverPlayerEntity)
 	{
-		if (player.getBlockPos().equals(SpawnCommand.config.getSpawnPos()))
+		if (serverPlayerEntity.getBlockPos().equals(SpawnCommand.config.getSpawnPos()) &&
+			serverWorld2.getRegistryKey().getValue().toString().equals(SpawnCommand.config.getWorld(serverPlayerEntity.getServer()).getValue().toString()))
 		{
 			// jank fix for MC-200092
-			SpawnCommand.execute(player.getCommandSource());
+			SpawnCommand.execute(serverPlayerEntity.getCommandSource());
 		}
 	}
 }
